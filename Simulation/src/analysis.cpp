abeeb60af0feb9a912853d9bd9ae388df6b7e486
ArnavT005 <arnavtuli00@gmail.com>
2021-05-19 16:41:13 +0530

    Add csv file

--- Simulation/src/analysis.cpp
+++ Simulation/src/analysis.cpp
@@ -5,6 +5,7 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_mixer.h>
 #include <vector>
+#include <fstream>
 #include "window.hpp"
 #include "maze.hpp"
 #include "ghost.hpp"
@@ -140,135 +141,132 @@ int main(int argc, char** argv) {
         return 0;
     }
 
-    srand(time(0));
+    ofstream fout("Analysis.csv");
+    fout << "Optimal,Experimental\n";
+    int counter = 0;
 
-    std::string bgType = std::to_string((rand() % 6 + 1));
-    Maze maze(16, 45, 15, 25);
-    maze.bgType = bgType;
-    SDL_Color boundaryColor = {0xFF, 0x00, 0x00, 0xFF};
-    SDL_Color startPointColor = {0x00, 0xFF, 0x00, 0xFF};
-    SDL_Color endPointColor = {0xFF, 0x00, 0x00, 0xFF};
-    SDL_Color eatableColor = {0x00, 0x00, 0xFF, 0xFF};
-    SDL_Texture* background = SDL_CreateTexture(window.getRenderer(), SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 1025, 1025);
-   
-    SDL_Rect bg = {0, 0, 1025, 1025};
-    SDL_Rect scr = {1025, 0, 380, 1025};
-    SDL_SetTextureBlendMode(background, SDL_BLENDMODE_BLEND);
-    maze.loadTexture(&window);
+    while(counter < 100) {
 
-    window.setRenderTarget(background);
-   
-    maze.createBase(&window, 0, 0, boundaryColor);
-    maze.generateMazeRandom(&window);
+        srand(time(0));
 
-    int i = 0;
-    while(!maze.BFS() && i < 16) {
-        maze.refreshMaze(&window);
+        std::string bgType = std::to_string((rand() % 6 + 1));
+        Maze maze(16, 45, 15, 25);
+        maze.bgType = bgType;
+        SDL_Color boundaryColor = {0xFF, 0x00, 0x00, 0xFF};
+        SDL_Color startPointColor = {0x00, 0xFF, 0x00, 0xFF};
+        SDL_Color endPointColor = {0xFF, 0x00, 0x00, 0xFF};
+        SDL_Color eatableColor = {0x00, 0x00, 0xFF, 0xFF};
+        SDL_Texture* background = SDL_CreateTexture(window.getRenderer(), SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 1025, 1025);
+       
+        SDL_Rect bg = {0, 0, 1025, 1025};
+        SDL_Rect scr = {1025, 0, 380, 1025};
+        SDL_SetTextureBlendMode(background, SDL_BLENDMODE_BLEND);
+        maze.loadTexture(&window);
+
+        window.setRenderTarget(background);
+       
         maze.createBase(&window, 0, 0, boundaryColor);
         maze.generateMazeRandom(&window);
-        i ++;
-    }
 
-    // generate starting and ending positions
-    int startX = rand() % 16;
-    int startY = rand() % 16;
-    int endX = rand() % 16;
-    int endY = rand() % 16;
-
-    maze.colourBlock(startX, startY, startPointColor, &window, false);
-    if(endX == startX && endY == startY) {
-    	maze.colourBlock(endX, endY, endPointColor, &window, true);
-	}
-	else {
-    	maze.colourBlock(endX, endY, endPointColor, &window, false);		
-	}
-
-    // choose 6 locations for eatables
-    std::vector<std::pair<int, int>> locations(6, std::make_pair(-1, -1));
-    for(int i = 0; i < 6; i ++) {
-    	locations[i].first = rand() % 16;
-    	locations[i].second = rand() % 16;
-    	while(isSame(startX, startY, endX, endY, locations, i)) {
-    		locations[i].first = rand() % 16;
-    		locations[i].second = rand() % 16;
-    	}
-    	maze.colourBlock(locations[i].first, locations[i].second, eatableColor, &window, false);
-    }
-	//find all distances using BFS
-	map<pair<int, int>, int> distance;
-	for(int k = 0; k < 6; k++){
-		BFS(locations[k].first, locations[k].second, k, locations, startX, startY, endX, endY, distance, &maze);
-	}
-	vector<vector<int>> allPermutations = findPermutations(6);
-	//find optimal distance
-	int optimalDist = 9999;
-    std::vector<int> optimalPerm;
-	for(int i = 0; i < allPermutations.size(); i++){
-        int d = findDistance(allPermutations[i], distance);
-        if(optimalDist >  d) {
-            optimalDist = d;
-            optimalPerm = allPermutations[i];
+        int i = 0;
+        while(!maze.BFS() && i < 16) {
+            maze.refreshMaze(&window);
+            maze.createBase(&window, 0, 0, boundaryColor);
+            maze.generateMazeRandom(&window);
+            i ++;
         }
 
-	}
-	cout << optimalDist << "\n";
-    for(int i = 0; i < 6; i ++) {
-        std::cout << locations[optimalPerm[i]].first << " " << locations[optimalPerm[i]].second << "\n";
-    }
-    window.setRenderTarget(NULL);
+        // generate starting and ending positions
+        int startX = rand() % 16;
+        int startY = rand() % 16;
+        int endX = rand() % 16;
+        int endY = rand() % 16;
 
-    // create ghost
-    Ghost ghost(&maze, &window, startX, startY);
+        maze.colourBlock(startX, startY, startPointColor, &window, false);
+        if(endX == startX && endY == startY) {
+        	maze.colourBlock(endX, endY, endPointColor, &window, true);
+    	}
+    	else {
+        	maze.colourBlock(endX, endY, endPointColor, &window, false);		
+    	}
 
-    Eatable item;
-    // tell locations to ghost
-    for(int i = 0; i < 6; i ++) {
-    	ghost.dest.push_back(locations[i]);
-    	SDL_Point point = maze.getBlockScreenCoordinate(locations[i].first, locations[i].second);
-    	item.setValues(point.x + 7, point.y + 7, 31, 31, FIFTY_POINT, &window);
-    	ghost.large.push_back(item);
-    }
-    ghost.dest.push_back(std::make_pair(endX, endY));
-
-    SDL_Event event;
-    Mix_Chunk* startGame = Mix_LoadWAV("../music/intro.wav");
-    if(startGame == NULL) {
-        std::cout << "Failed to load music! SDL_Mixer Error: " << Mix_GetError() << "\n";
-    }
-    else {
-        if(Mix_PlayChannel(0, startGame, 0) == -1) {
-            std::cout << "Error in playing music! SDL_Mixer Error: " << Mix_GetError() << "\n";
-        }    
-    }
-
-    bool quit = false;
-    Uint32 start = SDL_GetTicks();
-
-    while(!quit) {
-        while(SDL_PollEvent(&event)) {
-            if(event.type == SDL_QUIT || (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)) {
-                quit = true;
-                break;
+        // choose 6 locations for eatables
+        std::vector<std::pair<int, int>> locations(6, std::make_pair(-1, -1));
+        for(int i = 0; i < 6; i ++) {
+        	locations[i].first = rand() % 16;
+        	locations[i].second = rand() % 16;
+        	while(isSame(startX, startY, endX, endY, locations, i)) {
+        		locations[i].first = rand() % 16;
+        		locations[i].second = rand() % 16;
+        	}
+        	maze.colourBlock(locations[i].first, locations[i].second, eatableColor, &window, false);
+        }
+    	//find all distances using BFS
+    	map<pair<int, int>, int> distance;
+    	for(int k = 0; k < 6; k++){
+    		BFS(locations[k].first, locations[k].second, k, locations, startX, startY, endX, endY, distance, &maze);
+    	}
+    	vector<vector<int>> allPermutations = findPermutations(6);
+    	//find optimal distance
+    	int optimalDist = 9999;
+        std::vector<int> optimalPerm;
+    	for(int i = 0; i < allPermutations.size(); i++){
+            int d = findDistance(allPermutations[i], distance);
+            if(optimalDist >  d) {
+                optimalDist = d;
+                optimalPerm = allPermutations[i];
             }
-        } 
-        if(SDL_GetTicks() - start >= 4000) {
+
+    	}
+    	fout << optimalDist << ",";
+        // for(int i = 0; i < 6; i ++) {
+        //     std::cout << locations[optimalPerm[i]].first << " " << locations[optimalPerm[i]].second << "\n";
+        // }
+        window.setRenderTarget(NULL);
+
+        // create ghost
+        Ghost ghost(&maze, &window, startX, startY);
+
+        Eatable item;
+        // tell locations to ghost
+        for(int i = 0; i < 6; i ++) {
+        	ghost.dest.push_back(locations[i]);
+        	SDL_Point point = maze.getBlockScreenCoordinate(locations[i].first, locations[i].second);
+        	item.setValues(point.x + 7, point.y + 7, 31, 31, FIFTY_POINT, &window);
+        	ghost.large.push_back(item);
+        }
+        ghost.dest.push_back(std::make_pair(endX, endY));
+
+        SDL_Event event;
+
+        bool quit = false;
+
+        while(!quit && !ghost.end) {
+            while(SDL_PollEvent(&event)) {
+                if(event.type == SDL_QUIT || (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)) {
+                    quit = true;
+                    break;
+                }
+            } 
             ghost.update(&window);
             ghost.move();
-        }    
-        window.clearWindow();
-        window.renderTexture(background, NULL, &bg);
-	    if(ghost.targetAcquired) {
-	        for(int i = 0; i < ghost.small.size(); i ++) {
-	        	ghost.small[i].render(&window);
-	        }
-	    }
-        for(int i = 0; i < ghost.large.size(); i ++) {
-        	ghost.large[i].render(&window);
+            window.clearWindow();
+            window.renderTexture(background, NULL, &bg);
+    	    if(ghost.targetAcquired) {
+    	        for(int i = 0; i < ghost.small.size(); i ++) {
+    	        	ghost.small[i].render(&window);
+    	        }
+    	    }
+            for(int i = 0; i < ghost.large.size(); i ++) {
+            	ghost.large[i].render(&window);
+            }
+            ghost.render(&window);
+            window.updateWindow(); 
         }
-        ghost.render(&window);
-        window.updateWindow(); 
-    }
-    std::cout << ghost.distance << "\n";
+        fout << ghost.distance << "\n";
+        counter ++;
+    }  
+    fout.close();  
     window.free();
     close();
 }
