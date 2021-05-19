754029914c09b7a4889a845ce6fcac1fe15f5e11
ArnavT005 <arnavtuli00@gmail.com>
2021-05-19 16:17:39 +0530

    Update analysis file

--- Simulation/src/analysis.cpp
+++ Simulation/src/analysis.cpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <map>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_mixer.h>
@@ -46,6 +47,85 @@ bool isSame(int startX, int startY, int endX, int endY, std::vector<std::pair<in
 		}
 	}
 	return false;
+}
+
+
+void BFS(int destX, int destY, int k, std::vector<std::pair<int, int>> &locations, int startX, int startY, int endX, int endY, std::map<std::pair<int, int>, int> &distance, Maze* maze){
+    int size = maze->dimension;
+	std::vector<std::vector<int>> m(size, std::vector<int>(size, -1));
+    if(destX < 0 || destX >= size || destY < 0 || destY >= size){
+        return;
+    }
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            m[i][j] = -1;
+        }
+    }
+    m[destX][destY] = 0;
+    std::vector<std::vector<int>> que;
+    que.push_back(std::vector<int> {destX, destY});
+    while(!que.empty()){
+        std::vector<int> pt = que[0];
+        que.erase(que.begin());
+        if(maze->maze[pt[0]][pt[1]].up != ALL_DENIED && maze->maze[pt[0]][pt[1]].up != GHOST_DENIED && pt[0] > 0 && m[pt[0]-1][pt[1]] == -1){
+            m[pt[0]-1][pt[1]] = m[pt[0]][pt[1]] + 1;
+            que.push_back(std::vector<int> {pt[0]-1, pt[1]});
+        }
+        if(maze->maze[pt[0]][pt[1]].down != ALL_DENIED && maze->maze[pt[0]][pt[1]].down != GHOST_DENIED && pt[0] < size - 1 && m[pt[0]+1][pt[1]] == -1){
+            m[pt[0]+1][pt[1]] = m[pt[0]][pt[1]] + 1;
+            que.push_back(std::vector<int> {pt[0]+1, pt[1]});
+        }
+        if(maze->maze[pt[0]][pt[1]].right != ALL_DENIED && maze->maze[pt[0]][pt[1]].right != GHOST_DENIED && pt[1] < size - 1 && m[pt[0]][pt[1]+1] == -1){
+            m[pt[0]][pt[1]+1] = m[pt[0]][pt[1]] + 1;
+            que.push_back(std::vector<int> {pt[0], pt[1]+1});
+        }
+        if(maze->maze[pt[0]][pt[1]].left != ALL_DENIED && maze->maze[pt[0]][pt[1]].left != GHOST_DENIED && pt[1] > 0 && m[pt[0]][pt[1]-1] == -1){
+            m[pt[0]][pt[1]-1] = m[pt[0]][pt[1]] + 1;
+            que.push_back(std::vector<int> {pt[0], pt[1]-1});
+        }
+    }
+	for(int i = 0; i < 6; i++){
+		distance.insert({make_pair(i,k), m[locations[i].first][locations[i].second]});
+		distance.insert({make_pair(k,i), m[locations[i].first][locations[i].second]});
+	}
+	distance.insert({make_pair(k,-1), m[startX][startY]});
+	distance.insert({make_pair(-1,k), m[startX][startY]});
+	distance.insert({make_pair(k,6), m[endX][endY]});
+	distance.insert({make_pair(6,k), m[endX][endY]});
+
+}
+
+std::vector<std::vector<int>> findPermutations(int n){
+	std::vector<std::vector<int>> list = {{0}};
+	for(int i = 1; i < n; i++){
+		std::vector<std::vector<int>> temp ={};
+		for(int j = 0; j < list.size(); j++){
+			for(int k = 0; k < list[j].size() + 1; k++){
+				int flag = 0; std::vector<int> permute = {};
+				for(int m = 0; m < list[j].size() + 1; m++){
+					if(m == k){ permute.push_back(i); flag = 1;}
+					else{permute.push_back(list[j][m - flag]);}
+				}
+				temp.push_back(permute);
+			}
+		}
+		list = temp;
+	}
+	return list;
+}
+
+int findDistance(std::vector<int> &v, map<std::pair<int, int>, int> &distance){
+	int ans = 0;
+    int i = 0;
+	for(; i < v.size()-1; i++){
+		auto iter =  distance.find(std::make_pair(v[i],v[i+1]));
+		ans+=iter->second;
+	}
+	auto iter =  distance.find(std::make_pair(v[0],-1));
+	ans+=iter->second;
+	iter =  distance.find(std::make_pair(v[i],6));
+	ans+=iter->second;
+	return ans;
 }
 
 int main(int argc, char** argv) {
@@ -114,7 +194,27 @@ int main(int argc, char** argv) {
     	}
     	maze.colourBlock(locations[i].first, locations[i].second, eatableColor, &window, false);
     }
+	//find all distances using BFS
+	map<pair<int, int>, int> distance;
+	for(int k = 0; k < 6; k++){
+		BFS(locations[k].first, locations[k].second, k, locations, startX, startY, endX, endY, distance, &maze);
+	}
+	vector<vector<int>> allPermutations = findPermutations(6);
+	//find optimal distance
+	int optimalDist = 9999;
+    std::vector<int> optimalPerm;
+	for(int i = 0; i < allPermutations.size(); i++){
+        int d = findDistance(allPermutations[i], distance);
+        if(optimalDist >  d) {
+            optimalDist = d;
+            optimalPerm = allPermutations[i];
+        }
 
+	}
+	cout << optimalDist << "\n";
+    for(int i = 0; i < 6; i ++) {
+        std::cout << locations[optimalPerm[i]].first << " " << locations[optimalPerm[i]].second << "\n";
+    }
     window.setRenderTarget(NULL);
 
     // create ghost
@@ -167,9 +267,8 @@ int main(int argc, char** argv) {
         }
         ghost.render(&window);
         window.updateWindow(); 
-        SDL_Delay(15);
     }
-
+    std::cout << ghost.distance << "\n";
     window.free();
     close();
 }
